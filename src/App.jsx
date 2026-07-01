053c71e84fa931c5422735812e47e4c6c4602af7
Enzo Santoni <enzo.santoni@gmail.com>
2026-07-01 10:41:23 -0300

    feat: search and weather fetch (#1)
    * feat: add SearchBar component with controlled input
    
    * feat: add useWeather hook with loading/error
    
    * feat: connect

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,29 @@ import { Routes, Route } from 'react-router-dom'
 import WeatherView from './components/WeatherView'
 import GameView from './components/GameView'
 import CompareView from './components/CompareView'
+import SearchBar from './components/SearchBar'
+import { useWeather } from './hooks/useWeather'
+
+
 
 function App() {
+  const [city, setCity] = useState('')
+  const {data, loading, error} = useWeather(city)
+  function onSearch(c) {
+    setCity(c)
+  }
+
+
 
   return (
+    <div className="container">
+      <SearchBar onSearch={onSearch} />
     <Routes>
       <Route path="/" element={<WeatherView />}></Route>
       <Route path="/compare" element={<CompareView />}></Route>
       <Route path="/game" element={<GameView />}></Route>
     </Routes>
+    </div>
   )
 }
 
