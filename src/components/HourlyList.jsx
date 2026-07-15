252baf654c4b7949c2219ef5269e98673eea141c
Enzo Santoni <enzo.santoni@gmail.com>
2026-07-15 10:44:09 -0300

    feat: add forecast list and hourly breakdown

--- src/components/HourlyList.jsx
+++ src/components/HourlyList.jsx
@@ -1,15 +1,23 @@
-export default function HourlyList({selectedDay, forecast}) {
-    if (!selectedDay) return null
+import { useEffect, useState } from "react";
 
-    const fDay = forecast.list.filter((f) => f.dt_txt.startsWith(selectedDay))
+export default function HourlyList({selectedDay, forecast}) {
+
+    const [lastDay, setLastDay] = useState("")
+
+    useEffect(() => {
+        if(selectedDay) setLastDay(selectedDay)
+    }, [selectedDay])
+
+    const fDay = selectedDay || lastDay ? forecast.list.filter((f) => f.dt_txt.startsWith(selectedDay || lastDay)) :[];
+    
 
     return (
-        <div className="forecastHoursContainer">
+        <div className={selectedDay ? "forecast-hours-container open" : "forecast-hours-container"}>
             {fDay.map((d) => {
-                return <div className="forecastHours" key={d.dt_txt}>
-                    <div>{d.dt_txt.split(" ")[1].split(":").slice(0, 2).join(":")}</div>
-                    <img src={`https://openweathermap.org/img/wn/${d.weather[0].icon}@2x.png`} alt="Image of weather by hour" />
-                    <div>{Math.round(d.main.temp)}°</div>
+                return <div className="forecast-hours" key={d.dt_txt}>
+                    <div className="hours-time">{d.dt_txt.split(" ")[1].split(":").slice(0, 2).join(":")}</div>
+                    <img className="hours-img" src={`https://openweathermap.org/img/wn/${d.weather[0].icon}@2x.png`} alt="Imagen del clima" />
+                    <div className="hours-temp">{Math.round(d.main.temp)}°</div>
                 </div>
             })}
         </div>
